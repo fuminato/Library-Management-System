25e9c624887ea05b036b07f8d955166aab9c4c2f
vinhle-HCMUT <164595842+vinhle-HCMUT@users.noreply.github.com>
2025-05-30 09:16:39 +0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+/*import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import Catalog from './pages/Catalog.jsx';
+const router = createBrowserRouter([
+  {path: '/', element: <App />},
+  {path: '/dashboard', element: <App />},
+  {path: '/catalog', element: <Catalog />},
+  {path: '/books', element: <Books />},
+  {path: '/users', element: <Users />},
+  //{path: '/logout', element: <App />},
+]);
+*/
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+
     <App />
   </StrictMode>,
 )
